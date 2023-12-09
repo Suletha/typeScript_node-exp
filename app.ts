c58297e0f01304552a1ddae4af0fc963b217f213
suletha <suletha.gopi@gmail.com>
2023-12-09 07:33:57 +0530

    node/exp

---
+++ app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import todosRouter from './routes/todos';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(todosRouter)
+app.listen(300)
